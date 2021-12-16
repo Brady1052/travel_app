a01f8dc8fdf802c9c65caabaa08af0609a5dad94
Brady Buchholz <bradybuchholz@gmail.com>
2021-12-16 11:25:18 -0500

    testing branch

---
+++ client/src/components/Homepage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Homepage = () => {
+    return (
+        <div>
+            <h1 className='title'>Travel App</h1>
+            <h1> Test</h1>
+        </div>
+        
+    )
+}
+
+export default Homepage
