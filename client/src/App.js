edfef6559091f036582a2b22bb34c3cdc9b40f02
Brady Buchholz <bradybuchholz@gmail.com>
2021-12-17 12:58:35 -0500

    added ground. Still working on styling and adding city names to photos

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
-import Navbar from './components/Navbar';
+import NavTabs from './components/NavTabs';
 import Homepage from './components/Pages/Homepage';
-import Images from './components/Images';
+
 function App() {
   return (
     <div className='main'>
-     <Navbar />
      <Homepage />
-     <Images />
     </div>
   );
 }
