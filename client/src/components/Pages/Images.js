98fee51257035f7963c2d2a114042bdc60f1a74a
Brady Buchholz <bradybuchholz@gmail.com>
2021-12-17 13:51:24 -0500

    images added to homepage

--- client/src/components/Pages/Images.js
+++ client/src/components/Pages/Images.js
@@ -12,6 +12,9 @@ const Images = () => {
     <img src="https://media.istockphoto.com/photos/aerial-view-of-port-vell-barcelona-catalonia-spain-picture-id1320141457?b=1&k=20&m=1320141457&s=170667a&w=0&h=Yh82k8cVPCcvrwUq2u-N3lvdYHEv52R3qk4nzoXJYKI=" alt='barcelona'></img>
     <img src="https://media.istockphoto.com/photos/new-york-city-nyc-usa-picture-id615398376?b=1&k=20&m=615398376&s=170667a&w=0&h=v59Pfuvn4jTqysv0RYKsYhoUM_ayitgQIsG8cFg3rM8=" alt='New York City'></img>
     <img src="https://media.istockphoto.com/photos/bay-bridge-and-san-francisco-skyline-at-sunset-picture-id476881195?b=1&k=20&m=476881195&s=170667a&w=0&h=mEHJq7rrrlPYy4JmegDVGIBrQ8QW21IFmV0EdSQowPI="  alt='San Francisco'></img>
+    <img src="https://media.istockphoto.com/photos/sunrise-over-rice-terraces-bali-mountains-and-volcano-are-on-the-picture-id1310879355?b=1&k=20&m=1310879355&s=170667a&w=0&h=jEMgA5nOzxeJ3gNhD-Cz2XjrM6Jh62GKwb8Jr0y4Le0="  alt='Bali'></img>
+    <img src="https://media.istockphoto.com/photos/mt-fuji-and-tokyo-skyline-picture-id904453184?b=1&k=20&m=904453184&s=170667a&w=0&h=rwzxuUV5F3Sr8qm9eqE09ZrknCIGe2ZgKmyxo2fIDgo=" alt="Tokyo"></img>
+    <img src="https://media.istockphoto.com/photos/stockholm-aerial-panorama-over-gamla-stan-city-waterfront-landmarks-picture-id900786948?b=1&k=20&m=900786948&s=170667a&w=0&h=nRjoECJJugwiJLhRBwEyCr2Z6V4ZNQhKHLlyS2P2RAM=" alt="Stockholm"></img>
   </div>  
         </div>
         </div>
