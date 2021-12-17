01e3da4dc12dde34dc1ecac246c831de51e97054
Brady Buchholz <bradybuchholz@gmail.com>
2021-12-17 10:32:21 -0500

    added photos to the homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
-// import 'index.css';
 import Navbar from './components/Navbar';
 import Homepage from './components/Pages/Homepage';
-
+import Images from './components/Images';
 function App() {
   return (
     <div className='main'>
      <Navbar />
      <Homepage />
+     <Images />
     </div>
   );
 }
---
+++ client/src/components/Images.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Images = () => {
+    return (
+        <div>
+          <div class="row"> 
+  <div class="column">
+    <img src="https://media.istockphoto.com/photos/miami-skyline-yachts-sail-on-sea-water-to-city-picture-id1260383704?b=1&k=20&m=1260383704&s=170667a&w=0&h=xeNFCivY0NDPBj2KInQsQyIHzHEXxBdJwNp8PV2y7IA="  alt='miami'></img>
+    <img src="https://media.istockphoto.com/photos/the-main-attraction-of-paris-and-all-of-europe-is-the-eiffel-tower-in-picture-id1185953092?b=1&k=20&m=1185953092&s=170667a&w=0&h=piovAtT7yPx3v8ovL64iQZJrfF8VMedyyOLf2AuRSUU=" alt='paris'></img>
+    <img src="https://media.istockphoto.com/photos/aerial-view-of-london-and-the-tower-bridge-picture-id1265900812?b=1&k=20&m=1265900812&s=170667a&w=0&h=jOR9G-887Sh5LXygPFBIy8TIx5vCoVDm8FKISpapq-E="  alt='London'></img>
+    <img src="https://media.istockphoto.com/photos/aerial-view-of-port-vell-barcelona-catalonia-spain-picture-id1320141457?b=1&k=20&m=1320141457&s=170667a&w=0&h=Yh82k8cVPCcvrwUq2u-N3lvdYHEv52R3qk4nzoXJYKI=" alt='barcelona'></img>
+    <img src="https://media.istockphoto.com/photos/new-york-city-nyc-usa-picture-id615398376?b=1&k=20&m=615398376&s=170667a&w=0&h=v59Pfuvn4jTqysv0RYKsYhoUM_ayitgQIsG8cFg3rM8=" alt='New York City'></img>
+    <img src="https://media.istockphoto.com/photos/bay-bridge-and-san-francisco-skyline-at-sunset-picture-id476881195?b=1&k=20&m=476881195&s=170667a&w=0&h=mEHJq7rrrlPYy4JmegDVGIBrQ8QW21IFmV0EdSQowPI="  alt='San Francisco'></img>
+    <img src="https://media.istockphoto.com/photos/houston-texas-usa-skyline-picture-id1004243142?b=1&k=20&m=1004243142&s=170667a&w=0&h=1dDe9sOFH8VJLMkw93qFFQ5-7kwRSPqUEFGXrj_HCrs="  alt='Houston'></img>
+  </div>  
+        </div>
+        </div>
+    )
+}
+
+export default Images
