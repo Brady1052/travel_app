12652317da07d479fcf0e7c92115d796f58bce4a
Matthew Dukeshire <mdukeshire07@gmail.com>
2021-12-17 13:16:59 -0500

    added visited.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Homepage from './components/Pages/Homepage';
 import Images from './components/Images';
-import Visited from './components/Pages/Visited';
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
      <Navbar />
      <Homepage />
      <Images />
-     <Visited />
     </div>
   );
 }
