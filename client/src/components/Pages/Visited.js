54198644fe411f8b63b061904c83536d4bee9aff
Matthew Dukeshire <mdukeshire07@gmail.com>
2021-12-17 14:32:36 -0500

    Visited Page

--- client/src/components/Pages/Visited.js
+++ client/src/components/Pages/Visited.js
@@ -1,10 +1,14 @@
 import React from 'react'
-
+import Images from './Images'
 const Visited = () => {
     return (
-        <div>
+        <div
+            style={{
+            backgroundImage: `url("https://images.pexels.com/photos/255464/pexels-photo-255464.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500")`
+        }}>
             <h1 className='title'>Places You Have Visited</h1>
-            <h1> Test</h1>
+            <Images />
+            <Images />
         </div>
         
     )
