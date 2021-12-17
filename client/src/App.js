1ef58f040c78d9ea6929f303f17960c727e31536
Matthew Dukeshire <mdukeshire07@gmail.com>
2021-12-17 13:01:29 -0500

    Added Visited.js page

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,18 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Homepage from './components/Pages/Homepage';
 import Images from './components/Images';
+import Visited from './components/Pages/Visited';
+
 function App() {
   return (
     <div className='main'>
      <Navbar />
      <Homepage />
      <Images />
+     <Visited />
     </div>
   );
 }
+
 
 export default App;
