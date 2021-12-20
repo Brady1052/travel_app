e4b6eab3ef9a91d19b6f09a400228be891e0b059
Brady Buchholz <bradybuchholz@gmail.com>
2021-12-20 11:27:31 -0500

    renders different buttons when logged out or logged in

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import React from 'react'
 import AppContainer from './components/AppContainer';
+import Profile from './components/Profile';
+import LogoutButton from './components/LogoutButton';
+import LoginButton from './components/LoginButton';
 
 function App() {
     return (
--- client/src/components/LoginButton.js
+++ client/src/components/LoginButton.js
@@ -2,9 +2,14 @@ import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const LoginButton = () => {
-  const { loginWithRedirect } = useAuth0();
-
-  return <button className='button1' onClick={() => loginWithRedirect()}>Log In</button>;
+  const { loginWithRedirect, isAuthenticated } = useAuth0();
+  const {isLoading} = useAuth0();
+  if (isLoading) return <div>Loading...</div>
+  return (
+  !isAuthenticated && (
+  <button className='button1' onClick={() => loginWithRedirect()}>Log In</button>
+  )
+)
 };
 
 export default LoginButton;
