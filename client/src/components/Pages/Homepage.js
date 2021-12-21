af99679c96951c84ab3b706c0d2b49f6fd24f2fa
Ray Brown <raynarde.brown@gmail.com>
2021-12-20 23:53:48 -0500

    update homepage background and image text

--- client/src/components/Pages/Homepage.js
+++ client/src/components/Pages/Homepage.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Images from './Images'
+import heroImg from '../../img/hero-back.jpeg'
 
 const Homepage = () => {
     return (
         <div style={{
-            backgroundImage: `url("https://images.pexels.com/photos/255464/pexels-photo-255464.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500")`
+            backgroundImage: `url(${heroImg})`,
+            overflow: 'hidden'
+            
         }}>
             <h1 className='title'>Travel App</h1>
             <Images />
