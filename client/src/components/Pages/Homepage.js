678d7f6d1c6141f6168af78a1fd155a98319d7ee
Ray Brown <raynarde.brown@gmail.com>
2021-12-21 09:10:06 -0500

    main commit

--- client/src/components/Pages/Homepage.js
+++ client/src/components/Pages/Homepage.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import Images from './Images'
 import heroImg from '../../img/hero-back1.jpeg'
+import { Fade } from 'react-awesome-reveal'
 
 const Homepage = () => {
     return (
+        <Fade>
         <div style={{
             backgroundImage: `url(${heroImg})`,
-            overflow: 'hidden'
+            backgroundSize: 'cover'
             
         }}>
+            
             <h1 className='title'>Travel App</h1>
             <Images />
+            
         </div>
+        </Fade>
 
     )
 }
